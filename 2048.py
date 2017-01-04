3c71cf4f1e48eab5e9a4b981808a20cc3ef7afa5
yinuo <zhangkai890805@163.com>
2017-01-03 23:50:09 -0800

    2048.py

--- 2048.py
+++ 2048.py
@@ -20,9 +20,6 @@ def transpose(field):
 def invert(field):
 	return [row[::-1] for row in field]
 
-
-
-
 #create field
 
 class GameField(object):
@@ -33,7 +30,41 @@ class GameField(object):
 		self.score = 0		#当前分数
 		self.highscore = 0	#最高分
 		self.reset()		棋盘重置
-	
+
+	def reset(self):
+		if self.score > self.highscore:
+			self.highscore = self.score
+		self.score = 0
+		self.field = [[0 for i in range(self.width)] for j in range(self.height)]
+		self.spawn()
+		self.spawn()
+
+	def move(self, direction):
+		def move_row_left(row):
+			def tighten(row):#squeese non-zero elements together
+				new_row = [i for i in row if i != 0]
+				new_row += [0 for i in range(len(row) - len(new_row))]
+				return new_row
+
+			def merge(row):
+				pair = False
+				new_row = []
+				for i in range(len(row)):
+					if pair:
+						new_row.append(2 * row[i])
+						self.score += 2 * row[i]
+						pair = False
+					else:
+						if i + 1 < len(row) and row[i] == row[i + 1]:
+							pair = True
+							new_row.append(0)
+						else:
+							new_row.append(row[i])
+				assert len(new_row) == len(row)
+				return new_row
+			return tighten(merge(tighten(row)))
+
+
 
 
 #判断输赢
